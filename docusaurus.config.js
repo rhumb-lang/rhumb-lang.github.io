b2b7ec4b5461a26d69680ba2d176c0dc0d9cf6b8
Jake Russo <madcap.russo@gmail.com>
2023-05-16 17:09:36 -0500

    Update sub heading

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'The Rhumb Language',
-  tagline: 'Minimalist Prototypal Language',
+  tagline: 'Experimental International Language',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
