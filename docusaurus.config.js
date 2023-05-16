266c133aeac8caf1d079c7ce415273f8f975ca00
Jake Russo <madcap.russo@gmail.com>
2023-05-16 17:06:04 -0500

    Try to fix disappearing link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,7 +108,7 @@ const config = {
                 href: 'https://matrix.to/#/#rhumb-lang:matrix.org',
               },
               {
-                html: '<li class="footer__item"><a href="https://fosstodon.org/@rhumb" target="_blank" rel="me" class="footer__link-item">Mastodon <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></a></li>'
+                html: '<a href="https://fosstodon.org/@rhumb" target="_blank" rel="me" class="footer__link-item">Mastodon <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></a>'
               },
             ],
           },
